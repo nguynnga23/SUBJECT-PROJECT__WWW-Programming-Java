f543acc86b4f0a12402d9b237754d34c85175b0a
Nguyen Nga <nguyennga200x@gmail.com>
2024-12-05 20:06:01 +0700

    feat: Fake data

--- src/main/java/com/example/Week08JobApplication.java
+++ src/main/java/com/example/Week08JobApplication.java
@@ -1,14 +1,14 @@
 package com.example;
 
-import com.example.backend.models.Address;
-import com.example.backend.models.Candidate;
-import com.example.backend.models.User;
-import com.example.backend.repositories.AddressRepository;
-import com.example.backend.repositories.CandidateRepository;
-import com.example.backend.repositories.UserRepository;
+import com.example.backend.enums.SkillLevel;
+import com.example.backend.enums.SkillType;
+import com.example.backend.models.*;
+import com.example.backend.repositories.*;
 import com.example.backend.services.AddressService;
 import com.example.backend.services.CandidateService;
+import com.example.backend.services.UserService;
 import com.neovisionaries.i18n.CountryCode;
+import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,8 +16,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.security.Principal;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Random;
+import java.util.Set;
 
 @SpringBootApplication
 public class Week08JobApplication {
@@ -34,11 +38,19 @@ public class Week08JobApplication {
     @Autowired
     private AddressRepository addressRepository;
     @Autowired
-    private UserRepository userRepository;
+    private UserService userRepository;
+    @Autowired
+    private SkillRepository skillRepository;
+    @Autowired
+    private CandidateSkillRepository candidateSkillRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
     private BCryptPasswordEncoder passwordEncoder;
     @Bean
-    CommandLineRunner init() {
+    CommandLineRunner init(CompanyRepository companyRepository) {
         return args -> {
 //            Random rand = new Random();
 //            for (int i = 1; i <= 1; i++) {
@@ -59,14 +71,76 @@ public class Week08JobApplication {
 //                user.setPassword(passwordEncoder.encode("123"));
 //                userRepository.save(user);
 //                System.out.println("Added user " + user);
-            User user = userRepository.findByCandidateEmailOrCompanyEmail("email_1@gmail.com");
-            if (user != null) {
-                System.out.println("User found: " + user);
-                System.out.println("User email: " + user.getEmail());
-                System.out.println("User password: " + user.getPassword());
-            } else {
-                System.out.println("User not found");
-            }
+//            User user = userRepository.findByCandidateEmailOrCompanyEmail("email_1@gmail.com");
+//            if (user != null) {
+//                System.out.println("User found: " + user);
+//                System.out.println("User email: " + user.getEmail());
+//                System.out.println("User password: " + user.getPassword());
+//            } else {
+//                System.out.println("User not found");
+//            }
+
+
+//            Skill skill = new Skill();
+//            skill.setSkillName("Skill 1");
+//            skill.setSkillDescription("Skill 1 Description");
+//            skill.setType(SkillType.TECHNICAL_SKILL);
+//            skillRepository.save(skill);
+//            System.out.println(skill.toString());
+//            Candidate candidate = candidateRepository.findById(1L).get();
+//            System.out.println(candidate.toString());
+//            CandidateSkill candidateSkill = new CandidateSkill();
+//            candidateSkill.setCan(candidate);
+//            candidateSkill.setSkill(skill);
+//            candidateSkill.setSkillLevel(SkillLevel.ADVANCED);
+//            CandidateSkillId candidateSkillId = new CandidateSkillId();
+//            candidateSkillId.setSkillId(skill.getId());
+//            candidateSkillId.setCanId(candidate.getId());
+//            candidateSkill.setId(candidateSkillId);
+//            candidateSkillRepository.save(candidateSkill);
+//            System.out.println(candidateSkill.toString());
+//            Faker faker = new Faker();
+//            for(int i = 0; i < 100; i++){
+//                Company company = new Company();
+//                company.setEmail(faker.internet().emailAddress());
+//                company.setPhone(faker.phoneNumber().phoneNumber());
+//                company.setAbout(faker.company().profession());
+//                company.setCompName(faker.company().name());
+//                Address address = new Address();
+//                address.setCity(faker.address().city());
+//                address.setStreet(faker.address().streetAddress());
+//                address.setCity(faker.address().city());
+//                address.setZipcode(faker.address().zipCode());
+//                address.setCountry(CountryCode.VN);
+//                address.setNumber(faker.address().streetAddressNumber());
+//                addressRepository.save(address);
+//                company.setAddress(address);
+//                company.setWebUrl(faker.company().url());
+//                companyRepository.save(company);
+//                User user1 = new User();
+//                user1.setCompany(company);
+//                passwordEncoder = new BCryptPasswordEncoder();
+//                user1.setPassword(passwordEncoder.encode("company"));
+//                userRepository.save(user1);
+//            }
+//
+//            Set<Role> roles = (Set<Role>) roleRepository.findAll();
+//           for (User user : userRepository.findAll()) {
+//                if(user.getCompany() != null){
+//                    Optional<Role> role = roleRepository.findById(2l);
+//                    Set<Role> roles = new HashSet<>();
+//                    roles.add(role.get());
+//                    user.setRoles(roles);
+//                    userRepository.update(user, user.getId());
+//                }else{
+//                    Optional<Role> role = roleRepository.findById(1l);
+//                    Set<Role> roles = new HashSet<>();
+//                    roles.add(role.get());
+//                    user.setRoles(roles);
+//                    userRepository.update(user, user.getId());
+//                }
+//           }
+//
         };
     }
 }
